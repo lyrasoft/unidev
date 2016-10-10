3731f58acf3f17aa86bee66918700bfa8f38b5aa
Simon Asika <asika32764@gmail.com>
2016-10-10 15:19:17 +0800

    Auto create .idea when run bladeopt #3

--- src/Command/Unidev/BladeoptCommand.php
+++ src/Command/Unidev/BladeoptCommand.php
@@ -9,6 +9,7 @@
 namespace Lyrasoft\Unidev\Command\Unidev;
 
 use Windwalker\Core\Console\CoreCommand;
+use Windwalker\Filesystem\Folder;
 use Windwalker\Http\HttpClient;
 
 /**
@@ -18,15 +19,41 @@ use Windwalker\Http\HttpClient;
  */
 class BladeoptCommand extends CoreCommand
 {
+	/**
+	 * Property name.
+	 *
+	 * @var  string
+	 */
 	protected $name = 'bladeopt';
 
+	/**
+	 * Property description.
+	 *
+	 * @var  string
+	 */
 	protected $description = 'Install blade optimization for phpstorm';
 
+	/**
+	 * Property file.
+	 *
+	 * @var  string
+	 */
 	protected $file = 'https://raw.githubusercontent.com/lyrasoft/development-tools/master/Editor/PHPStorm/blade.xml';
 
+	/**
+	 * doExecute
+	 *
+	 * @return  bool
+	 */
 	protected function doExecute()
 	{
 		$dest = WINDWALKER_ROOT . '/.idea/blade.xml';
+
+		if (!is_dir(dirname($dest)))
+		{
+			Folder::create(dirname($dest));
+		}
+
 		$http = new HttpClient;
 		$http->download($this->file, $dest);
 
