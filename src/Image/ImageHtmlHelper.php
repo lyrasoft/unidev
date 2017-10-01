1d76de05a0b8292c074fbae9396304f91fc4dbf1
Simon Asika <asika32764@gmail.com>
2017-10-01 21:40:03 +0800

    Add bgThumb() and defaultImage() #26

---
+++ src/Image/ImageHtmlHelper.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Part of earth project.
+ *
+ * @copyright  Copyright (C) 2017 ${ORGANIZATION}.
+ * @license    __LICENSE__
+ */
+
+namespace Lyrasoft\Unidev\Image;
+
+use Lyrasoft\Unidev\UnidevPackage;
+use Windwalker\Core\Asset\Asset;
+use Windwalker\Core\Package\PackageHelper;
+use Windwalker\Dom\HtmlElement;
+use Windwalker\Utilities\Arr;
+
+/**
+ * The ImageHtmlHelper class.
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class ImageHtmlHelper
+{
+	/**
+	 * bgThumb
+	 *
+	 * @param string     $image
+	 * @param int|string $width
+	 * @param int|string $height
+	 * @param string     $sizeType
+	 * @param array      $attribs
+	 *
+	 * @return string
+	 */
+	public static function bgThumb($image = null, $width = 50, $height = 50, $sizeType = 'cover', $attribs = [])
+	{
+		if (!$image)
+		{
+			$image = static::defaultImage();
+		}
+
+		$width = is_numeric($width) ? $width . 'px' : $width;
+		$height = is_numeric($height) ? $height . 'px' : $height;
+
+		$image = static::addBase(htmlentities($image, ENT_QUOTES, 'UTF-8'));
+
+		$style = "width: $width; height: $height; background-image: url('$image'); background-size: $sizeType; background-position: center center; background-repeat: no-repeat;";
+		$attribs['style'] = Arr::get($attribs, 'style') . ';' . $style;
+		$attribs['class'] = Arr::get($attribs, 'class') . ' sq-thumb';
+
+		return new HtmlElement('div', '', $attribs);
+	}
+
+	/**
+	 * defaultImage
+	 *
+	 * @return  string
+	 */
+	public static function defaultImage()
+	{
+		$alias = PackageHelper::getAlias(UnidevPackage::class);
+
+		return Asset::root() . '/' . $alias . '/images/default-img.png';
+	}
+
+	/**
+	 * addBase
+	 *
+	 * @param string $url
+	 *
+	 * @return  string
+	 */
+	public static function addBase($url)
+	{
+		if (strpos($url, 'http') === 0 || strpos($url, '//') === 0)
+		{
+			return $url;
+		}
+
+		return Asset::root() . ltrim($url, '/');
+	}
+}
