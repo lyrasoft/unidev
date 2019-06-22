029541d22d33f869561ab4cd38960f5a8e9a2316
Simon Asika <asika32764@gmail.com>
2019-06-22 20:24:18 +0800

    Use new SlugHelper

--- src/Seo/SlugHelper.php
+++ src/Seo/SlugHelper.php
@@ -8,60 +8,14 @@
 
 namespace Lyrasoft\Unidev\Seo;
 
-use Windwalker\Core\DateTime\Chronos;
-use Windwalker\Filter\OutputFilter;
-
 /**
  * The SlugHelper class.
  *
  * @since  1.3
+ *
+ * @deprecated  Use \Phoenix\Utilities\SlugHelper instead.
  */
-class SlugHelper
+class SlugHelper extends \Phoenix\Utilities\SlugHelper
 {
-    /**
-     * Make slug safe.
-     *
-     * @param string   $title
-     * @param bool     $utf8
-     * @param int|null $limit
-     *
-     * @return  string
-     * @throws \Exception
-     */
-    public static function safe($title, $utf8 = false, ?int $limit = 8)
-    {
-        $slug = static::slugify($title, $utf8, $limit);
-
-        if (trim($slug) === '') {
-            $slug = OutputFilter::stringURLSafe(Chronos::current('Y-m-d-H-i-s'));
-        }
-
-        return $slug;
-    }
-
-    /**
-     * slugify
-     *
-     * @param string   $title
-     * @param bool     $utf8
-     * @param int|null $limit
-     *
-     * @return  string
-     */
-    public static function slugify($title, $utf8 = false, ?int $limit = 8)
-    {
-        if ($limit) {
-            $title = str($title)->truncate($limit)->__toString();
-        }
-        
-        if ($utf8) {
-            return OutputFilter::stringURLUnicodeSlug($title);
-        }
-
-        if (function_exists('transliterator_transliterate')) {
-            $title = transliterator_transliterate('Any-Latin; Latin-ASCII; Lower()', $title);
-        }
-
-        return OutputFilter::stringURLSafe($title);
-    }
+    //
 }
